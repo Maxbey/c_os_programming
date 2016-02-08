e8c23726c2998ee611255b74c6f54f4bc1cf7979
Maxbey <maxbeiner@gmail.com>
2016-02-08 17:56:11 +0600

    Fix mistake in unix/stream_helpers.c

--- helpers/unix/stream_helpers.c
+++ helpers/unix/stream_helpers.c
@@ -3,7 +3,7 @@
 #include "stream_helpers.h"
 
 void stream_write(int handle, char *buff){
-    write(handle, buff, strlen(line));
+    write(handle, buff, strlen(buff));
 }
 
 void stream_read(int handle, char *buff, int bytes_cnt){
