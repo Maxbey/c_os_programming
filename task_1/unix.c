cc4e1029c7ac880befaa0f4507dae834313adc02
maxbey <maxbeiner@gmail.com>
2016-02-20 17:37:58 +0600

    Dolved alvanced part of first task

--- task_1/unix.c
+++ task_1/unix.c
@@ -1,27 +1,22 @@
-#include "../helpers/unix/stdhandles.h"
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 void print_line(int, char *);
 
-int main(){
-    char input_buffer[255] = "";
-    ssize_t readed;
+int main() {
+  char input_buffer[255] = "";
+  ssize_t readed;
+  if(isatty(0) !=0 )
+    print_line(2, "Type any text: ");
 
-    print_line(OUTPUT_HANDLE, "The unix standart input handle is: 0\n");
-    print_line(OUTPUT_HANDLE, "The unix standart output handle is: 1\n");
-    print_line(OUTPUT_HANDLE, "The unix standart error handle is: 2\n");
+  readed = read(0, input_buffer, sizeof(input_buffer));
+  print_line(1, "Typed text: ");
+  write(1, input_buffer, readed);
 
-    print_line(OUTPUT_HANDLE, "\nJust type any text: ");
-
-    readed = read(INPUT_HANDLE, input_buffer, sizeof(input_buffer));
-    print_line(OUTPUT_HANDLE, "\nTyped text: ");
-    write(OUTPUT_HANDLE, input_buffer, readed);
-
-    return 0;
+  return 0;
 }
 
 void print_line(int handle, char *line){
-    write(handle, line, strlen(line));
-}
+  write(handle, line, strlen(line));
+};
