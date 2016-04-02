6add81945c4f76793eeee11688acc8b0c90c02c0
maxbey <maxbeiner@gmail.com>
2016-04-02 22:33:27 +0600

    Updated task 8

--- task_8/unix.c
+++ task_8/unix.c
@@ -7,11 +7,18 @@ pthread_mutex_t output_mutex;
 sem_t s_sem, w_sem;
 int readers = 0;
 
+struct thread_config {
+  int position;
+  int delay;
+};
+
 char buff[12] = "++++++++++++";
 
-void reader_thread(void *arg){
+void reader_thread(void *args){
   char format[255];
   int iter = 0;
+
+  struct thread_config *config = args;
 
   for (int i = 0; i < 12; i++){
     sem_wait(&s_sem);
@@ -22,7 +29,7 @@ void reader_thread(void *arg){
 
     sem_post(&s_sem);
 
-    sprintf(format, "\033[%d;%dH", i + 1, (int) arg);
+    sprintf(format, "\033[%d;%dH", i + 1, config->position);
     printf("%s", format);
     printf("%c", buff[i]);
     printf("\n");
@@ -35,7 +42,7 @@ void reader_thread(void *arg){
 
     sem_post(&s_sem);
 
-    usleep(600000);
+    usleep(config->delay * 100000);
 
     if(i == 11 && iter < 3){
         i = 0;
@@ -52,7 +59,7 @@ void writer_thread1(void *arg){
     sem_wait(&w_sem);
 
     strncpy(buff, content, 6);
-    usleep(600000);
+    usleep((int)arg * 100000);
     strncpy(buff, content, 12);
 
     sem_post(&w_sem);
@@ -68,7 +75,7 @@ void writer_thread2(void *arg){
     sem_wait(&w_sem);
 
     strncpy(buff, content, 6);
-    usleep(600000);
+    usleep((int)arg * 100000);
     strncpy(buff, content, 12);
 
     sem_post(&w_sem);
@@ -83,7 +90,7 @@ void writer_thread3(void *arg){
     sem_wait(&w_sem);
 
     strncpy(buff, content, 5);
-    usleep(600000);
+    usleep((int)arg * 100000);
     strncpy(buff, content, 10);
 
     sem_post(&w_sem);
@@ -96,20 +103,33 @@ void main(){
   pthread_t r_tid1, r_tid2, r_tid3;
   pthread_t w_tid1, w_tid2, w_tid3;
 
-  pthread_mutex_init(&mutex, NULL);
+  pthread_mutex_init(&output_mutex, NULL);
 
   sem_init(&w_sem, 0, 1);
 	sem_init(&s_sem, 0, 1);
 
   printf("\033[2J\n");
 
-  pthread_create(&r_tid1, NULL, (void*)reader_thread, (void*)5);
-  pthread_create(&r_tid2, NULL, (void*)reader_thread, (void*)15);
-  pthread_create(&r_tid3, NULL, (void*)reader_thread, (void*)25);
+  struct thread_config reader1_config;
+  struct thread_config reader2_config;
+  struct thread_config reader3_config;
 
-  pthread_create(&w_tid1, NULL, (void*)writer_thread1, (void*)1);
-  pthread_create(&w_tid2, NULL, (void*)writer_thread2, (void*)1);
-  pthread_create(&w_tid3, NULL, (void*)writer_thread3, (void*)1);
+  reader1_config.position = 5;
+  reader1_config.delay = 1;
+
+  reader2_config.position = 15;
+  reader2_config.delay = 2;
+
+  reader3_config.position = 25;
+  reader3_config.delay = 3;
+
+  pthread_create(&r_tid1, NULL, (void*)reader_thread, (void *) &reader1_config);
+  pthread_create(&r_tid2, NULL, (void*)reader_thread, (void *) &reader2_config);
+  pthread_create(&r_tid3, NULL, (void*)reader_thread, (void *) &reader3_config);
+
+  pthread_create(&w_tid1, NULL, (void*)writer_thread1, (void*)4);
+  pthread_create(&w_tid2, NULL, (void*)writer_thread2, (void*)5);
+  pthread_create(&w_tid3, NULL, (void*)writer_thread3, (void*)6);
 
   getchar();
 }
